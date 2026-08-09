a9bfa3df813d6cb471eac14c71919bce500a1b23
Aman <saxenaaman555@gmail.com>
2026-08-09 21:13:19 +0530

    initial commit

---
+++ src/main/java/com/dochive/dochive_backend/DochiveBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dochive.dochive_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DochiveBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DochiveBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dochive/dochive_backend/DochiveBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dochive.dochive_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DochiveBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
